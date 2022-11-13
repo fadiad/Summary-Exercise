363bca7f50c69379764f3f999c9dbaa691ca8dab
Fadiad <fadi.1997.id@gmail.com>
2022-11-13 21:11:30 +0200

    last edit , everything is working , but need to build the  logic

--- src/main/java/mainB/Main.java
+++ src/main/java/mainB/Main.java
@@ -1,24 +1,17 @@
 package mainB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import mainB.db.Repo;
+
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/sonoo", "root", "");
+//        Repo repo = new Repo("sonoo");
+//        repo.makeQuery("select * from emp");
 
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        Repo repo = new Repo("asd");
+        repo.makeDB();
+
+
     }
 }
---
+++ src/main/java/mainB/db/Repo.java
@@ -0,0 +1,61 @@
+package mainB.db;
+
+import java.sql.*;
+
+public class Repo {
+    private final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private final String USER = "root";
+    private final String PASS = "";
+    private static Connection conn;
+    private static Statement stmt;
+
+    public Repo(String DBName) {
+        makeConnection();
+        makeDB();
+        makeTable("stam");
+    }
+
+    public void makeConnection() {
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeDB() {
+        try {
+            String makeTableQuery = "CREATE DATABASE  IF NOT EXISTS ORMDB";
+            stmt.executeUpdate(makeTableQuery);
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeQuery(String query) {
+        try {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeTable(String tableName) {
+        try {
+            conn = DriverManager.getConnection(DB_URL + "ORMDB", USER, PASS);
+            stmt = conn.createStatement();
+
+            String makeTableQuery = "CREATE TABLE " + tableName + "(id int,email varchar (255), password varchar(255));";
+
+            stmt.executeUpdate(makeTableQuery);
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
