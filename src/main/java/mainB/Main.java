f60bd0adf5691cf3e2899f79207972d17e6d8d71
Fadiad <fadi.1997.id@gmail.com>
2022-11-14 09:28:47 +0200

    editing errors

--- src/main/java/mainB/Main.java
+++ src/main/java/mainB/Main.java
@@ -9,8 +9,23 @@ public class Main {
 //        Repo repo = new Repo("sonoo");
 //        repo.makeQuery("select * from emp");
 
-        Repo repo = new Repo("asd");
-        repo.makeDB();
+//        Repo repo = new Repo("asd");
+//        repo.makeDB();
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/sonoo", "root", "");
+//here sonoo is database name, root is username and password
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from emp");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
 
     }
